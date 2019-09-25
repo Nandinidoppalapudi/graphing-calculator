0c05265899c5c0d35a324e0ff9b9924dfcd5287c
student <student@nw45513.nwmissouri.edu>
2019-09-25 16:21:26 -0500

    Changed

---
+++ graphing-calculator/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  graphing-calculator
+//
+//  Created by student on 9/25/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Hello Nandini
+    }
+
+
+}
+
---
+++ graphing-calculator/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  graphing-calculator
+//
+//  Created by student on 9/25/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
