d1afcf7989a587b6d6314f74e740022396affa97
student <student@nw45513.nwmissouri.edu>
2019-10-04 15:39:05 -0500

    Added content to info page

--- graphing-calculator/InfoViewController.swift
+++ graphing-calculator/InfoViewController.swift
@@ -10,21 +10,50 @@ import UIKit
 
 class InfoViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    var strings:[String] = []
+    @IBOutlet weak var bulletLabel: UITextView!
+    
+    @IBOutlet weak var textLabel: UITextView!
+    
+    override func viewDidLoad()
+    {
+    super.viewDidLoad()
+    
+    let bullet1 = "Slope-Intercept  : y=mx+b  where m is slope and b is y-intercept\n"
+    let bullet2 = "Point-Slope  : y-y1=m(x-x1)  where m is  slope  and  (x, y) is point on the line.\n"
+    let bullet3 = "Standard :   Ax+By=C where A,B,Care constants\n"
+    
+    strings = [bullet1, bullet2, bullet3]
+    
+    var fullString = ""
+    
+    for string: String in strings
+    {
+    let bulletPoint: String = "\u{2022}"
+    let formattedString: String = "\(bulletPoint) \(string)\n"
+    
+    fullString = fullString + formattedString
     }
     
+    bulletLabel.text = fullString
+    
+    let bullet4 = "Cubic Equation: 3x^3+a2x^2+a1x+a0==0 \n"
+    let bullet5 = "Trignometric Equation: sin(x) + cos(x) = 1\n"
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    strings = [bullet4, bullet5]
+    
+    var fullString1 = ""
+    
+    for string2: String in strings
+    {
+    let bulletPoint1: String = "\u{2022}"
+    let formattedString1: String = "\(bulletPoint1) \(string2)\n"
+    
+    fullString1 = fullString1 + formattedString1
     }
-    */
+    
+    textLabel.text = fullString1
 
+}
 }
