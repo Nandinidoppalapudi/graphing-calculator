5af4058463a738de8d48381610f1f3578d41b9d6
Dustin Potter <dustinpotter@nw46155.nwmissouri.edu>
2019-10-04 16:35:30 -0500

    Connected variables to text fields in graph tab

--- graphing-calculator/GraphViewController.swift
+++ graphing-calculator/GraphViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class GraphViewController: UIViewController {
 
+
+    @IBOutlet weak var x1TF: UITextField!
+    @IBOutlet weak var y1TF: UITextField!
+    @IBOutlet weak var x2TF: UITextField!
+    @IBOutlet weak var y2TF: UITextField!
+    @IBOutlet weak var yinterceptTF: UITextField!
+    @IBOutlet weak var slopeTF: UITextField!
+    @IBOutlet weak var aTF: UITextField!
+    @IBOutlet weak var bTF: UITextField!
+    @IBOutlet weak var cTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
