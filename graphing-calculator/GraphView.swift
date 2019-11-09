0457151f071505d56e02397cc311c9b1dff04fda
Dustin Potter <s529827@nwmissouri.edu>
2019-11-09 12:58:03 -0600

    Created a function to draw a standard line equation that works with the scaling slider

--- graphing-calculator/GraphView.swift
+++ graphing-calculator/GraphView.swift
@@ -10,18 +10,21 @@ import UIKit
 
 class GraphView: UIView {
 
-    let xLength = 375
-    let yLength = 430
+    let xLength = 375.0
+    let yLength = 430.0
     
-    let xHalf = 375/2
-    let yHalf = 430/2
+    let xHalf = 375.0/2.0
+    let yHalf = 430.0/2.0
     
-    var max = 10
-    var min = 10
+    var getHashValue = 1.0
     
+    var slope: Double? = nil
+    var yIntercept: Double? = nil
     
     
     override func draw(_ rect: CGRect) {
+        let originalHashx = xHalf/10.0
+        let originalHashy = yHalf/10.0
         
         let yLine = UIBezierPath()
         yLine.lineWidth = 1.0
@@ -40,13 +43,13 @@ class GraphView: UIView {
         let xHash = UIBezierPath()
         xHash.lineWidth = 1.0
         UIColor.black.setStroke()
-        for i in stride(from: xHalf, through: 0, by: -(xHalf)/min)  {
+        for i in stride(from: xHalf, through: 0, by: -originalHashx)  {
            xHash.move(to: CGPoint(x: i, y: yHalf + 5))
            xHash.addLine(to: CGPoint(x: i, y: yHalf - 5))
            xHash.stroke()
         }
         
-        for i in stride(from: xHalf, through: xLength, by: xHalf/max) {
+        for i in stride(from: xHalf, through: xLength, by: originalHashx) {
             xHash.move(to: CGPoint(x: i, y: yHalf + 5))
             xHash.addLine(to: CGPoint(x: i, y: yHalf - 5))
             xHash.stroke()
@@ -55,18 +58,49 @@ class GraphView: UIView {
         let yHash = UIBezierPath()
         yHash.lineWidth = 1.0
         UIColor.black.setStroke()
-        for i in stride(from: yHalf, through: 0, by: -(yHalf)/max){
+        for i in stride(from: yHalf, through: 0, by: -originalHashy){
             yHash.move(to: CGPoint(x: xHalf - 5, y: i))
             yHash.addLine(to: CGPoint(x: xHalf + 5, y: i))
             yHash.stroke()
         }
         
-        for i in stride(from: yHalf, through: yLength, by: yHalf/min) {
+        for i in stride(from: yHalf, through: yLength, by: originalHashy) {
             yHash.move(to: CGPoint(x: xHalf - 5, y: i))
             yHash.addLine(to: CGPoint(x: xHalf + 5, y: i))
             yHash.stroke()
             
-            //test
+        }
+        
+        if slope != nil && yIntercept != nil {
+            drawLine(slope: slope!, yIntercept: yIntercept!)
         }
     }
+        
+        func drawLine(slope: Double, yIntercept: Double) {
+            let originalHashx = xHalf/10.0
+            let originalHashy = yHalf/10.0
+
+            let funcLine = UIBezierPath()
+            funcLine.lineWidth = 1.0
+            UIColor.blue.setStroke()
+            
+            var counter1 = 1.0
+            
+            funcLine.move(to: CGPoint(x: xHalf, y: yHalf - ((originalHashy * yIntercept) / getHashValue)))
+            for point in stride(from: xHalf + originalHashx, through: xLength * getHashValue, by: originalHashx) {
+                funcLine.addLine(to: CGPoint(x: point, y: yHalf - ((originalHashy * yIntercept) + (originalHashy * slope * counter1)) / getHashValue))
+                counter1 += 1
+            }
+            funcLine.stroke()
+            
+            var counter2 = 1.0
+            
+            funcLine.move(to: CGPoint(x: xHalf, y: yHalf - ((originalHashy * yIntercept) / getHashValue)))
+            for point in stride(from: xHalf - originalHashx, through: 0, by: -originalHashx) {
+                funcLine.addLine(to: CGPoint(x: point, y: yHalf - ((originalHashy * yIntercept) - (originalHashy * slope * counter2)) / getHashValue))
+                counter2 += 1
+            }
+            funcLine.stroke()
+ 
+        }
 }
--- graphing-calculator/GraphViewController.swift
+++ graphing-calculator/GraphViewController.swift
@@ -15,25 +15,64 @@ class GraphViewController: UIViewController {
     @IBOutlet weak var y1TF: UITextField!
     @IBOutlet weak var x2TF: UITextField!
     @IBOutlet weak var y2TF: UITextField!
+    @IBOutlet weak var pointsEquationLBL: UILabel!
+    
     @IBOutlet weak var yinterceptTF: UITextField!
     @IBOutlet weak var slopeTF: UITextField!
+    
     @IBOutlet weak var aTF: UITextField!
     @IBOutlet weak var bTF: UITextField!
     @IBOutlet weak var cTF: UITextField!
+    
     @IBOutlet weak var graphView: GraphView!
     
-    @IBAction func changeScale(_ sender: UIButton) {
-        self.graphView.max = Int(scaleX.text!)!
-        self.graphView.min = Int(scaleY.text!)!
-        self.graphView.setNeedsDisplay()
+    @IBOutlet weak var getHashValue: UISlider!
+    @IBOutlet weak var hashNumberLBL: UILabel!
+    
+    @IBAction func clearGraph(_ sender: UIButton) {
+        x1TF.text = nil
+        y1TF.text = nil
+        x2TF.text = nil
+        y2TF.text = nil
+        pointsEquationLBL.text = "y = mx + b"
+        yinterceptTF.text = nil
+        graphView.yIntercept = Double(yinterceptTF.text!)
+        slopeTF.text = nil
+        graphView.slope = Double(slopeTF.text!)
+        aTF.text = nil
+        bTF.text = nil
+        cTF.text = nil
+        graphView.setNeedsDisplay()
     }
     
-    @IBOutlet weak var scaleX: UITextField!
-    @IBOutlet weak var scaleY: UITextField!
+    @IBAction func graphPointsEquation(_ sender: UIButton) {
+    }
     
+    @IBAction func graphStandardEquation(_ sender: UIButton) {
+        graphView.slope = Double(slopeTF.text!)
+        graphView.yIntercept = Double(yinterceptTF.text!)
+        graphView.setNeedsDisplay()
+    }
+    
+    @IBAction func graphQuadraticEquation(_ sender: UIButton) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        getHashValue.addTarget(self, action: #selector(GraphViewController.slideValueChanged(_:)), for: .valueChanged)
+        
+        graphView.getHashValue = Double(getHashValue.value)
+        hashNumberLBL.text = String(getHashValue.value)
+        pointsEquationLBL.text = "y = mx + b"
+    }
+    
+    @objc func slideValueChanged(_ send: UISlider!) {
+        let roundedValue = roundf(send.value)
+        send.value = roundedValue
+        graphView.getHashValue = Double(getHashValue.value)
+        hashNumberLBL.text = String(getHashValue.value)
+        graphView.setNeedsDisplay()
     }
 
 }
